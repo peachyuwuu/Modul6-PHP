118f312ce3fa846e81526bf92646dd8cacbd0f68
peachyuwuu <jonte55@live.se>
2023-04-17 14:06:59 +0200

    second commit

--- app/public/index.php
+++ app/public/index.php
@@ -1,12 +1,23 @@
+<?php
+
+
+
+// En variabel i PHP inleds med dollartecken
+$title = "En webbsida med PHP";
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="sv">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo $title; ?></title>
 </head>
 <body>
+
+    <h1><?php echo "Hello world"; ?></h1>
     
 </body>
 </html>
---
+++ app/public/phpinfo.php
@@ -0,0 +1,8 @@
+<?php
+
+// Använd den inbyggda funktionen phpinfo() för att visa info om php
+phpinfo();
+
+
+
+?>
